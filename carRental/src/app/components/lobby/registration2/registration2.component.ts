a144b7dcf87f5376691d92cc85e899bcb448b9c7
markwartman1 <markwartman1@gmail.com>
2020-07-27 16:02:27 -0500

    template driven form HTML with invalid styling

--- carRental/src/app/components/lobby/registration2/registration2.component.ts
+++ carRental/src/app/components/lobby/registration2/registration2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registration2',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Registration2Component implements OnInit {
 
+  answer = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+
+    if(!form.valid)
+    return form;
+
+    console.log("username: "+form.value.username);
+    console.log("eMail: "+form.value.email);
+    console.log("questionAnswer: "+form.value.questionAnswer);
+    form.resetForm;
   }
 
 }
