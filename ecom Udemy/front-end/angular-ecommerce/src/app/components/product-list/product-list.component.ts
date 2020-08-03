3d21763593aedf567adc3df1ab2a728ffe07c928
markwartman1 <markwartman1@gmail.com>
2020-08-03 13:25:56 -0500

    added product category to the frontend

--- ecom Udemy/front-end/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecom Udemy/front-end/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/models/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -11,16 +12,31 @@ import { Product } from 'src/app/models/product';
 export class ProductListComponent implements OnInit {
 
   products: Product[];
+  currentCategoryId: number;
 
-  constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
 
-    this.listProducts();
+    this.route.paramMap.subscribe(() => this.listProducts());
+
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+
+    // checks if 'id' parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if(hasCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    }
+    else {
+      this.currentCategoryId = 1;
+    }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => this.products = data 
     );
   }
--- ecom Udemy/front-end/angular-ecommerce/src/app/services/product.service.ts
+++ ecom Udemy/front-end/angular-ecommerce/src/app/services/product.service.ts
@@ -3,29 +3,47 @@ import { HttpClient } from "@angular/common/http";
 import { Product } from '../models/product';
 import { Observable } from 'rxjs';
 import { map } from "rxjs/operators";
+import { ProductCategory } from '../models/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
+  
   private baseUrl = 'http://localhost:8080/api/products';
-
+  private categoryUrl = 'http://localhost:8080/api/product-category';
+  
   constructor(private http: HttpClient) { }
-
+  
   // getProductList(): Observable<Product[]> {
-  //   return this.http.get<Product[]>(this.baseUrl);
-  // }
+    //   return this.http.get<Product[]>(this.baseUrl);
+    // }
+    
+    getProductList(theCategoryId: number): Observable<Product[]> {
+      
+      const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+      
+      return this.http.get<GetResponseProducts>(searchUrl).pipe(
+        map(response => response._embedded.products)
+        );
+      }
 
-  getProductList(): Observable<Product[]> {
-    return this.http.get<GetResponse>(this.baseUrl).pipe(
-      map(response => response._embedded.products)
-    );
-  }
-}
+      getProductCategories(): Observable<ProductCategory[]> {
+        return this.http.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+          map(response => response._embedded.productCategory)
+          );
+      }
+    }
+    
+    interface GetResponseProducts {
+      _embedded: {
+        products: Product[];
+      }
+    }
 
-interface GetResponse {
-  _embedded: {
-    products: Product[];
-  }
-}
+    interface GetResponseProductCategory {
+      _embedded: {
+        productCategory: ProductCategory[];
+      }
+    }
+    
