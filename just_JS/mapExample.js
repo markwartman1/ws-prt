7b0a84b7d13c23497d0e7766bf93e42dab061fe8
markwartman1 <markwartman1@gmail.com>
2020-08-07 05:28:46 -0500

    map-object example

---
+++ just_JS/mapExample.js
@@ -0,0 +1,26 @@
+
+function exampleMap() {
+    let recipeMap = new Map([
+        ['cucumber', 500],
+        ['tomatoes', 350],
+        ['onion', 50]
+    ]);
+
+    // iterate over keys (vegetables)
+    for (let vegetable of recipeMap.keys()) {
+        console.log(vegetable); // cucumber, tomatoes, onion
+    }
+
+    // iterate over values (amounts)
+    for (let amount of recipeMap.values()) {
+        console.log(amount); // 500, 350, 50
+    }
+
+    // iterate over [key, value] entries
+    for (let entry of recipeMap) { // the same as of recipeMap.entries()
+        console.log(entry); // cucumber,500 (and so on)
+    }
+
+}
+
+console.log("this would return undefined: " + exampleMap());
