85a00b7065a8bed51cf8df0ebba7878f6c4ab1c5
markwartman1 <markwartman1@gmail.com>
2020-08-17 13:43:33 -0500

    cart-details calculations

---
+++ ecom Udemy/front-end/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/models/cart-item';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart-details',
+  templateUrl: './cart-details.component.html',
+  styleUrls: ['./cart-details.component.css']
+})
+export class CartDetailsComponent implements OnInit {
+
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+
+    this.listCartDetails();
+  }
+
+  listCartDetails() {
+    
+    this.cartItems = this.cartService.cartItems;
+
+    this.cartService.totalPrice.subscribe(data => this.totalPrice = data);
+
+    this.cartService.totalQuantity.subscribe(data => this.totalQuantity = data);
+
+    // not sure if this is needed
+    this.cartService.computeCartTotals();
+  }
+
+}
