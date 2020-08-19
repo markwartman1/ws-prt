cebe021de4fc22acfff7f759aa58873e2cf9dc65
markwartman1 <markwartman1@gmail.com>
2020-08-19 16:53:15 -0500

    form advancement

--- ecom Udemy/front-end/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecom Udemy/front-end/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -19,8 +19,46 @@ export class CheckoutComponent implements OnInit {
         firstName: [''],
         lastName: [''],
         email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
       })
     });
+  }
+
+  onSubmit() {
+    console.log(this.checkoutFormGroup.get('customer').value);
+    
+  }
+
+  copyAddress(event) {
+    if(event.target.checked){
+      this.checkoutFormGroup.controls.billingAddress.setValue(
+        this.checkoutFormGroup.controls.shippingAddress.value
+      );
+    }
+    else {
+      this.checkoutFormGroup.controls.billingAddress.reset();
+    }
   }
 
 }
