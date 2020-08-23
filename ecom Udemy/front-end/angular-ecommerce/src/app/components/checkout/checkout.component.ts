d263dab2c13e06a4521eb3a7d806f4d194f094d2
markwartman1 <markwartman1@gmail.com>
2020-08-22 19:23:32 -0500

    month optimization credit card form

--- ecom Udemy/front-end/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecom Udemy/front-end/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -85,4 +85,26 @@ export class CheckoutComponent implements OnInit {
     }
   }
 
+  handleMonthsAndYears() {
+
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup.value.expirationYear);
+
+    let startMonth: number;
+
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    } else {
+      startMonth = 1;
+    }
+    this.formService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+  }
+
 }
