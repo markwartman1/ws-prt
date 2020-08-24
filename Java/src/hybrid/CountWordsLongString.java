9adff9808187cdce725a6aa84b7399facef47632
markwartman1 <markwartman1@gmail.com>
2020-08-24 11:05:57 -0500

    algorithm optimization

--- Java/src/hybrid/CountWordsLongString.java
+++ Java/src/hybrid/CountWordsLongString.java
@@ -14,28 +14,60 @@ public class CountWordsLongString {
 
 	}
 
+//	public static Map<String, Integer> count(String strings) {
+//
+//		Map<String, Integer> map = new HashMap();
+//		
+////		String[] thestrings = strings.split("\\.");
+//		String[] thestrings = strings.split(" ");
+//		System.out.println(thestrings.length);
+//
+//		for (String s : thestrings) {
+//			s = s.trim();
+//			s = s.replace(".", "");
+//			s = s.replace(",", "");
+//			s = s.replace("\'", "");
+//			String[] str = s.split(" ");
+//
+//			for(String i : str) {
+//				
+//				if (!map.containsKey(i)) {
+//					map.put(i, 1);
+//				} else {
+//					int val = map.get(i);
+//					val++;
+//					map.put(i, val);
+//				}
+//			}
+//		}
+//		System.out.println("just printing map");
+//		System.out.println(map);
+//		return map;
+//	}
+	
 	public static Map<String, Integer> count(String strings) {
 
 		Map<String, Integer> map = new HashMap();
 		
-		String[] thestrings = strings.split("\\.");
+//		String[] thestrings = strings.split("\\.");
+		String[] thestrings = strings.split(" ");
+		System.out.println(thestrings.length);
 
 		for (String s : thestrings) {
 			s = s.trim();
+			s = s.replace(".", "");
 			s = s.replace(",", "");
 			s = s.replace("\'", "");
-			String[] str = s.split(" ");
-
-			for(String i : str) {
+			
 				
-				if (!map.containsKey(i)) {
-					map.put(i, 1);
-				} else {
-					int val = map.get(i);
-					val++;
-					map.put(i, val);
-				}
+			if (!map.containsKey(s)) {
+				map.put(s, 1);
+			} else {
+				int val = map.get(s);
+				val++;
+				map.put(s, val);
 			}
+			
 		}
 		System.out.println("just printing map");
 		System.out.println(map);
