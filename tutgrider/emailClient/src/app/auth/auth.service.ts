55069e261139b286405aec63b925ffca4a242841
markwartman1 <markwartman1@gmail.com>
2020-09-21 12:00:46 -0500

    behavior subject authentication

--- tutgrider/emailClient/src/app/auth/auth.service.ts
+++ tutgrider/emailClient/src/app/auth/auth.service.ts
@@ -1,18 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  url = 'https://api.angular-email.com';
+  signedin$ = new BehaviorSubject(false);
+
   constructor(
     private http: HttpClient
   ) { }
 
   usernameAvailable(username: string) {
-    return this.http.post<{ available: boolean }>('https://api.angular-email.com/auth/username', {
+    return this.http.post<{ available: boolean }>(this.url + '/auth/username', {
       username: username
   });
+  }
+
+  signup(credentials: 
+    { 
+      username: string,
+      password: string,
+      passwordConfirmation: string
+    }) {
+    return this.http.post<{ username: string }>(this.url + '/auth/signup', credentials)
+      .pipe(tap( () => this.signedin$.next(true)));
   }
 }
