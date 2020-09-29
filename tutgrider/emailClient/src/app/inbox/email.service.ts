3056c87922181b313a91196ef39e0154f3098b24
markwartman1 <markwartman1@gmail.com>
2020-09-28 22:45:10 -0500

    service http get for emails and ngFor in Template

--- tutgrider/emailClient/src/app/inbox/email.service.ts
+++ tutgrider/emailClient/src/app/inbox/email.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+interface EmailSummary {
+  id: string,
+  subject: string;
+  from: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  constructor() { }
+  url = 'https://api.angular-email.com/emails';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getEmails() {
+    return this.http.get<EmailSummary[]>(this.url);
+  }
 }
